5b8ef0b1e7bca56e6980966280a53e5bf5228913
baltazarparra <baltazarparra@outlook.com>
2023-05-05 16:11:27 -0300

    add marquee

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import { Canvas, useFrame } from '@react-three/fiber'
 import { useGLTF, Float } from '@react-three/drei'
 import { Bloom, EffectComposer, ChromaticAberration, DotScreen, Noise } from '@react-three/postprocessing'
 import { Resizer, KernelSize, BlendFunction } from 'postprocessing'
+import { motion, useScroll, useSpring, useTransform, useMotionValue, useVelocity, useAnimationFrame } from 'framer-motion'
+import { wrap } from '@motionone/utils'
 
 function Model() {
   const { scene } = useGLTF('/smile.glb')
@@ -41,9 +43,53 @@ function Model() {
   )
 }
 
+function ParallaxText({ children, baseVelocity = 100 }) {
+  const baseX = useMotionValue(0)
+  const { scrollY } = useScroll()
+  const scrollVelocity = useVelocity(scrollY)
+  const smoothVelocity = useSpring(scrollVelocity, {
+    damping: 50,
+    stiffness: 400
+  })
+  const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
+    clamp: false
+  })
+  const x = useTransform(baseX, (v) => `${wrap(-20, -45, v)}%`)
+
+  const directionFactor = useRef(1)
+  useAnimationFrame((t, delta) => {
+    let moveBy = directionFactor.current * baseVelocity * (delta / 1000)
+
+    if (velocityFactor.get() < 0) {
+      directionFactor.current = -1
+    } else if (velocityFactor.get() > 0) {
+      directionFactor.current = 1
+    }
+
+    moveBy += directionFactor.current * moveBy * velocityFactor.get()
+
+    baseX.set(baseX.get() + moveBy)
+  })
+
+  return (
+    <div className="parallax">
+      <motion.div className="scroller" style={{ x }}>
+        <span>{children} </span>
+        <span>{children} </span>
+        <span>{children} </span>
+        <span>{children} </span>
+      </motion.div>
+    </div>
+  )
+}
+
 function App() {
   return (
     <>
+      <ParallaxText baseVelocity={1}>
+        React ━ Next.js ━ styled-components ━ strapi ━ Figma ━ agile ━ react-three-fiber ━ Framer Motion ━ React ━ Next.js ━
+        styled-components ━ strapi ━ Figma ━ agile ━ react-three-fiber ━ Framer Motion ━
+      </ParallaxText>
       <main>
         <h1 className="firstname">baltazar</h1>
         <div className="core">
@@ -77,16 +123,22 @@ function App() {
         </div>
         <h1 className="surname">parra</h1>
       </main>
-      <nav>
+      <nav className="bar">
         <ul>
           <li>
-            <a href="/">github</a>
+            <a href="https://github.com/baltazarparra" target="_blank" rel="noreferrer">
+              github
+            </a>
           </li>
           <li>
-            <a href="/">linkedIn</a>
+            <a href="https://www.linkedin.com/in/baltazarparra/" target="_blank" rel="noreferrer">
+              linkedIn
+            </a>
           </li>
           <li>
-            <a href="/">spotify</a>
+            <a href="https://open.spotify.com/album/6BFeIsMZ4zcuGbs5cugxLM?si=yWyn03SUQ_WhoSQtAqZzLQ" target="_blank" rel="noreferrer">
+              spotify
+            </a>
           </li>
         </ul>
       </nav>
