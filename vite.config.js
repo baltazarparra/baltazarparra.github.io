74756a6a4301a97db3f3d605c0a9901b6723ef22
baltazarparra <baltazarparra@outlook.com>
2023-05-05 07:45:53 -0300

    0.05

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
   server: {
     mimeTypes: {
-      'text/jsx': ['js']
+      'text/jsx': 'application/javascript'
     }
   }
 })
