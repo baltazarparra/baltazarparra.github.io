54d18889dbfe9e3a929392335367fd86a5e94d9e
baltazarparra <baltazar.parra@gft.com>
2024-03-25 16:29:01 -0300

    chore: initial markup and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 import "./App.css";
 
 function App() {
-  return <>baltazarparra</>;
+  return (
+    <section>
+      <header>
+        <h3>
+          baltazarparra
+          <img src="https://avatars.githubusercontent.com/u/7395304?v=4" />
+        </h3>
+        <span>― developer</span>
+        <ul>
+          <li>linkedIn</li>
+          <li>github</li>
+          <li>spotify</li>
+        </ul>
+      </header>
+      <main>
+        <h2>
+          <div>hey, I'm Baltz</div>
+          <div>a tech leader</div>
+          <div>based in Brasil</div>
+        </h2>
+        <h1>
+          I'm currently working for the{" "}
+          <a href="https://www.gft.com/int/en" target="_blank">
+            GFT group
+          </a>
+          , focused on create great experiences, enthusiastic about agile, with
+          extensive software engineering background
+        </h1>
+      </main>
+      <footer>baltazarparra ― 2024</footer>
+    </section>
+  );
 }
 
 export default App;
